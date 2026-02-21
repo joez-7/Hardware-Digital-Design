306ba4b59eb5bc0a94ba96969720fff367699d32
Joe Z <jz.joezhang@outlook.com>
2026-02-21 12:05:43 -0800

    Update format and descriptions

--- divide/divide_by_odd_50dc_tb.sv
+++ divide/divide_by_odd_50dc_tb.sv
@@ -1,30 +1,30 @@
 `timescale 1ps/1ps
 module tb();
-  reg clk, rst_n;
-  wire clk_out;
-  
-  initial begin
-  	#0 clk = 0;
+    reg clk, rst_n;
+    wire clk_out;
+
+initial begin
+    #0 clk = 0;
     forever #1 clk = ~clk;
-  end
-  
-  divide_by_odd_50dc #(
+end
+
+divide_by_odd_50dc #(
     .N (5)
-  ) dut (
+    ) dut (
     .clk (clk),
     .rst_n (rst_n),
     .clk_out (clk_out)
-  );
-  
-  initial begin
+);
+
+initial begin
     #0 rst_n = 0;
     #1 rst_n = 1;
     #50 $finish;
-  end
-  
-  initial begin
+end
+
+initial begin
     $dumpfile("wave.vcd");
     $dumpvars();
-  end
+end
   
 endmodule
